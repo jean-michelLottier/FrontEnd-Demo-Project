3b0367c7caa4269535289c9b754a016975dfcb89
Lottier <lottier.jm@gmail.com>
2018-02-04 15:19:28 +0100

    Stub home page before introduce business card feature

--- src/app/home/app.home.component.ts
+++ src/app/home/app.home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { AlertService } from '../alert/app.alert.service';
 
 @Component ({
     moduleId: module.id,
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './app.home.component.html'
 })
 export class HomeComponent implements OnInit {
-    constructor () {}
+    
+    constructor (private alertSerive: AlertService) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.alertSerive.clearAlert();
+    }
 }
